8b86218048889d3ede7afbe16cde986dcd43d1a5
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2024-09-18 13:52:16 +0300

    feat(aur): added checking installed program

--- scripts/aur.sh
+++ scripts/aur.sh
@@ -1,2 +1,5 @@
-sudo rm -rf /tmp/yay
-(cd /tmp && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && sudo rm -rf yay)
+if [[ ! $(which yay) ]]; then
+	sudo rm -rf /tmp/yay
+	(cd /tmp && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && sudo rm -rf yay)
+fi
+
