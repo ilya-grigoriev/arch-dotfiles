95ac1562592d1f75a8376f9e8cee72d1e16d5061
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2025-05-15 03:17:23 +0300

    feat(tools)

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -1,2 +1,6 @@
-sudo pacman -S fzf ripgrep plocate zoxide tmux htop github-cli lazygit zathura zathura-pdf-poppler zathura-djvu bc html2text
+sudo pacman -S fzf ripgrep plocate zoxide \
+	tmux htop github-cli lazygit \
+	zathura zathura-pdf-poppler zathura-djvu \
+	bc html2text \
+	wireshark-cli wireshark-qt
 yay -S zaread dragon-drop
